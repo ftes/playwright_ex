9969b979164e5d598f2f51205065d29525cafc22
Caio Peluti <cpeluti@gmail.com>
2026-05-14 19:04:31 -0300

    added flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "GO devenv for development";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config = {
+          allowUnfreePredicate =
+            pkg:
+            builtins.elem (pkgs.lib.getName pkg) [
+              "google-chrome"
+            ];
+        };
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nixfmt
+          beam27Packages.erlang # ← add this
+          beam27Packages.elixir
+          beam27Packages.elixir-ls # ← use beam27-specific elixir-ls
+          nodejs
+          yarn
+          postgresql
+          process-compose
+          inotify-tools
+          tailwindcss_4
+          tree
+          chromedriver
+          google-chrome
+          playwright-driver
+          playwright-driver.browsers
+          playwright-test
+        ];
+
+        shellHook = ''
+          export TAILWINDCSS_PATH="${pkgs.lib.getExe pkgs.tailwindcss_4}"
+          export CHROME_PATH="${pkgs.google-chrome}/bin/google-chrome-stable"
+          export CHROMEDRIVER_PATH="${pkgs.chromedriver}/bin/chromedriver"
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+        '';
+      };
+    };
+}
